c3f1872d4cb9613b4b7c51feb7ceb906e7856d8e
WaldoPeyrau <waldopeyrau@gmail.com>
2017-06-28 01:29:31 -0400

    Add files via upload

---
+++ ALumnos/Lista_Alumnos/Alumno.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Alumnos
+{
+    class Alumno
+    {
+        String _Rut;
+        public string Rut
+        {
+            get
+            {
+                return _Rut;
+            }
+
+            set
+            {
+                _Rut = value;
+            }
+        }
+        String _Nombre;
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+
+            set
+            {
+                _Nombre = value;
+            }
+        }
+        String _ApellidoPat;
+        public string ApellidoPat
+        {
+            get
+            {
+                return _ApellidoPat;
+            }
+
+            set
+            {
+                _ApellidoPat = value;
+            }
+        }
+        String _ApellidoMat;
+        public string ApellidoMat
+        {
+            get
+            {
+                return _ApellidoMat;
+            }
+
+            set
+            {
+                _ApellidoMat = value;
+            }
+        }
+        String _Direccion;
+        public string Direccion
+        {
+            get
+            {
+                return _Direccion;
+            }
+
+            set
+            {
+                _Direccion = value;
+            }
+        }
+        int _Telefono;
+        public int Telefono
+        {
+            get
+            {
+                return _Telefono;
+            }
+
+            set
+            {
+                _Telefono = value;
+            }
+        }
+
+        public bool validarTel(int tel)
+        {
+            Console.WriteLine("\nIngrese telfono : ");
+            if (tel == 0)
+            { 
+                while(!int.TryParse(Console.ReadLine(),out tel))
+                {
+                    Console.WriteLine("\nERROR !!! : Ingrese numero valido . . . ");
+                    Console.WriteLine("\nIngrese telfono : ");
+                }
+            }
+            return tel;
+        }
+
+        public int menu()
+        {
+            int opc;
+            Console.WriteLine("\nMENU");
+            Console.WriteLine("\n[ 1 ] Buscar por rut ");
+            Console.WriteLine("\n[ 2 ] Buscar por telefono ");
+            Console.WriteLine("\n[ 0 ] Salir ");
+            Console.WriteLine("\nElegir :  ");
+            while ((!int.TryParse(Console.ReadLine(), out opc)) || opc > 2 || opc < 0)
+            {
+                Console.WriteLine("\nERROR !! : Ingrese solo numeros o eliga una opcion valida . . . ");
+                Console.WriteLine("\nElegir : ");
+            }
+            return opc;
+        }
+
+        public bool validarRut(string rut)
+        {
+            bool validacion = false;
+            try
+            {
+                rut = rut.ToUpper();
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                int rutAux = int.Parse(rut.Substring(0, rut.Length - 1));
+
+                char dv = char.Parse(rut.Substring(rut.Length - 1, 1));
+
+                int m = 0, s = 1;
+                for (; rutAux != 0; rutAux /= 10)
+                {
+                    s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+                }
+                if (dv == (char)(s != 0 ? s + 47 : 75))
+                {
+                    validacion = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return validacion;
+        }
+
+
+    }
+}
---
+++ ALumnos/Lista_Alumnos/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Alumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Alumno> listAlumnos = new List<Alumno>();
+            Alumno objAlumno = new Alumno();
+            Console.WriteLine("\nINGRESAR 4 ALUMNOS ");
+            for(int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("\nIngrese datos de alumno " + (i + 1));
+                objAlumno = new Alumno();
+                Console.WriteLine("\nRut ( ejem : 25434190-8 ) : ");
+                string rut;
+                rut = Console.ReadLine();
+                int aux = 0; //variable de control
+                while (!objAlumno.validarRut(rut) || aux == 1)
+                {
+                    Console.WriteLine("\nERROR!! : Rut inválido o ya ingresado. . .");
+                    Console.WriteLine("\nRut ( ejem : 25434190-8 ) :");
+                    rut = Console.ReadLine();
+                    for (int j = 0; j < listAlumnos.Count; j++)
+                    {
+                        if (listAlumnos[j].Rut == rut)
+                        {
+                            aux = 1;
+                        }
+                    }
+                }
+                objAlumno.Rut = rut;
+                Console.WriteLine("\nNombre : ");
+                objAlumno.Nombre = Console.ReadLine();
+                Console.WriteLine("\nApellido Paterno : ");
+                objAlumno.ApellidoPat = Console.ReadLine();
+                Console.WriteLine("\nApellido Materno : ");
+                objAlumno.ApellidoMat= Console.ReadLine();
+                Console.WriteLine("\nDireccion : ");
+                objAlumno.Direccion = Console.ReadLine();
+
+                Console.WriteLine("\nTeléfono : ");
+                int tel;
+                while(!int.TryParse(Console.ReadLine(),out tel))
+				{
+					Console.WriteLine("\nERROR !!! : Ingrese numero valido . . . ");
+					Console.WriteLine("\nIngrese telfono : ");
+				}
+				while(!objAlumno.validarTel(tel))
+				{
+					Console.WriteLine("\nERROR !!! : Ingrese numero valido . . . ");
+					Console.WriteLine("\nIngrese telfono : ");
+				}
+				
+				objAlumno.Telefono = tel;
+				
+				
+                listAlumnos.Add(objAlumno);
+                Console.Clear();
+            }
+            int opc = objAlumno.menu();
+            while (opc != 0)
+            {
+                if(opc == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nIngrese rut para buscar ( ejem : 25434190-8 ) : ");
+                    string rut;
+                    rut = Console.ReadLine();
+                    while (rut == "")
+                    {
+                        Console.WriteLine("\nIngrese rut para buscar ( ejem : 25434190-8 ) : ");
+                        rut = Console.ReadLine();
+                    }
+                    int aux = 1;
+                    for (int i = 0; i < listAlumnos.Count; i++)
+                    {
+                        if (listAlumnos[i].Rut == rut && aux == 1)
+                        {
+                            Console.WriteLine("\nAlumno encontrado !!");
+                            Console.WriteLine("\nRut : " + listAlumnos[i].Rut);
+                            Console.WriteLine("\nNombre : " + listAlumnos[i].Nombre);
+                            Console.WriteLine("\nApellido Paterno : " + listAlumnos[i].ApellidoPat);
+                            Console.WriteLine("\nApellido Materno : " + listAlumnos[i].ApellidoMat);
+                            Console.WriteLine("\nDireccion : " + listAlumnos[i].Direccion);
+                            Console.WriteLine("\nTelefono : " + listAlumnos[i].Telefono);
+                            aux = 0;
+                        }
+                    }
+                    if (aux == 1)
+                    {
+                        Console.WriteLine("\nAlumno no encontrado !!! ");
+                    }
+                    Console.WriteLine("\nPresione una tecla para continuar  . . .");
+                    Console.ReadKey();
+                    Console.Clear();
+                    opc = objAlumno.menu();
+                }
+                else if(opc == 2)
+                {
+                    Console.Clear();
+                    int tel = 0;
+                    tel = objAlumno.validarTel(tel);
+                    int aux = 1;
+                    for (int i = 0; i < listAlumnos.Count; i++)
+                    {
+                        if (listAlumnos[i].Telefono == tel && aux == 1)
+                        {
+                            Console.WriteLine("\nAlumno encontrado !!");
+                            Console.WriteLine("\nRut : " + listAlumnos[i].Rut);
+                            Console.WriteLine("\nNombre : " + listAlumnos[i].Nombre);
+                            Console.WriteLine("\nApellido Paterno : " + listAlumnos[i].ApellidoPat);
+                            Console.WriteLine("\nApellido Materno : " + listAlumnos[i].ApellidoMat);
+                            Console.WriteLine("\nDireccion : " + listAlumnos[i].Direccion);
+                            Console.WriteLine("\nTelefono : " + listAlumnos[i].Telefono);
+                            aux = 0;
+                        }
+                    }
+                    if (aux == 1)
+                    {
+                        Console.WriteLine("\nAlumno no encontrado !!! ");
+                    }
+                    Console.WriteLine("\nPresione una tecla para continuar  . . .");
+                    Console.ReadKey();
+                    Console.Clear();
+                    opc = objAlumno.menu();
+                }
+            }
+        }
+    }
+}
